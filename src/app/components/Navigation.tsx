9c3e30a784872c4c6f68923ffdd59e3b2006a750
Roy Ketelaar <royketelaar@gmail.com>
2024-07-15 14:40:35 +0200

    refactor: Update Navigation component layout and styling

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -13,11 +13,14 @@ const Navigation = () => {
     });
   }, []);
   return (
-    <nav>
-      <ul>
+    <nav className="h-24">
+      <ul className="flex items-center justify-center space-x-4">
         {pages.map((page) => (
           <li key={page.sys.id}>
-            <a href={`/${page.fields.title.toLowerCase()}`}>
+            <a
+              href={`/${page.fields.title.toLowerCase()}`}
+              className="text-white hover:text-gray-300 font-bold"
+            >
               {page.fields.title.toLowerCase()}
             </a>
           </li>
