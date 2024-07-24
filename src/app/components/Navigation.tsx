3133885c666ffe3a6f1f0ce506662817a03b7441
Roy Ketelaar <royketelaar@gmail.com>
2024-07-24 21:56:38 +0200

    chore: Update npm dependencies and add dotenv package

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
 interface NavigationProps {
-  pages: string[];
+  pages: { title: string; slug: string }[];
 }
 
 const Navigation = ({ pages }: NavigationProps) => {
   return (
     <nav className="h-24">
       <ul className="flex items-center justify-center space-x-4">
-        {pages.map((title) => (
+        {pages.map(({ title, slug }) => (
           <li key={title}>
             <Link
-              href={`/${title}`}
+              href={`/${slug}`}
               className="text-white hover:text-gray-300 font-bold"
             >
               {title}
--- src/app/lib/contentful.ts
+++ src/app/lib/contentful.ts
@@ -1,14 +1,17 @@
 import * as contentful from "contentful";
 
+const spaceToken = process.env.CONTENTFUL_SPACE_TOKEN as string;
+const environmentToken = process.env.CONTENTFUL_ENVIRONMENT_TOKEN as string;
+const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN as string;
+
 const client = contentful.createClient({
-  ***REMOVED***
-  ***REMOVED***
-  ***REMOVED***
+  space: spaceToken,
+  environment: environmentToken,
+  accessToken: accessToken,
 });
 
 export const fetchEntryBySlug = async (slug: string) => {
   try {
-    console.log("slug", slug);
     const entry = await client.getEntries({
       content_type: "page",
       "fields.slug": slug,
@@ -26,9 +29,13 @@ export const fetchNavigationEntries = async () => {
   try {
     const entries = await client.getEntries({
       content_type: "page",
-      select: ["fields.title"],
-    });
-    return entries.items.map((item: any) => item.fields.title.toLowerCase());
+      select: ["fields.title", "fields.slug"],
+    })
+
+    return entries.items.map((item: any) => ({
+      title: item.fields.title.toLowerCase(),
+      slug: item.fields.slug,
+    }));
   } catch (error) {
     console.error(error);
     return [];
