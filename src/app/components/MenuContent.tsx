2dc871d402956596112714bdf748d4e8b6c8a995
Roy Ketelaar <royketelaar@gmail.com>
2024-10-29 18:44:00 +0100

    refactor: Improve MenuContent component structure and behavior

--- src/app/components/MenuContent.tsx
+++ src/app/components/MenuContent.tsx
@@ -19,35 +19,37 @@ const MenuContent = ({ pages, onClose }: NavigationProps) => {
         <ul>
           {pages.map(({ title, slug, childPages }) => (
             <li key={title}>
-              <Link
-                href={`/${slug}`}
-                className="text-sky-50 underlined-header text-2xl font-bold mb-2"
-                onClick={onClose}
-              >
-                {title.toUpperCase()}
-              </Link>
-              {childPages && (
-                <ul className="mb-8">
-                  {childPages.map((childPage: any) => (
-                    <li
-                      key={childPage?.fields?.slug}
-                      className="flex hover:underline"
-                    >
-                      <span className="inline-block min-h-3 min-w-3 max-h-3 max-w-3 bg-teal-400 rounded-full mr-2 mt-2"></span>
-                      <Link
-                        href={`/${childPage?.fields?.slug}`}
-                        className={`block text-sky-50 mb-2 text-lg ${
-                          currentUrl?.includes(childPage?.fields?.slug)
-                            ? "underline"
-                            : ""
-                        }`}
-                        onClick={onClose}
+              {childPages?.length && (
+                <>
+                  <Link
+                    href={`/${slug}`}
+                    className="text-sky-50 underlined-header text-2xl font-bold mb-2"
+                    onClick={onClose}
+                  >
+                    {title.toUpperCase()}
+                  </Link>
+                  <ul className="mb-8">
+                    {childPages.map((childPage: any) => (
+                      <li
+                        key={childPage?.fields?.slug}
+                        className="flex hover:underline"
                       >
-                        {childPage?.fields?.title}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
+                        <span className="inline-block min-h-3 min-w-3 max-h-3 max-w-3 bg-teal-400 rounded-full mr-2 mt-2"></span>
+                        <Link
+                          href={`/${childPage?.fields?.slug}`}
+                          className={`block text-sky-50 mb-2 text-lg ${
+                            currentUrl?.includes(childPage?.fields?.slug)
+                              ? "underline"
+                              : ""
+                          }`}
+                          onClick={onClose}
+                        >
+                          {childPage?.fields?.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                </>
               )}
             </li>
           ))}
